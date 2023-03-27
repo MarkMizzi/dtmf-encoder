d2c79595766b991be37a998b6fdffebe483f22ef
mmiz0052 <mizzimark2001@gmail.com>
2023-03-27 06:38:37 +0000

    Updated Doxygen documentation for the timer (SysTick wrapper) code.

--- code/src/timer.c
+++ code/src/timer.c
@@ -4,8 +4,20 @@
 #include <system_LPC407x_8x_177x_8x.h>
 #include <stddef.h>
 
+/** 
+ * Holds the interrupt invoked when the SysTick timer goes off.
+ */
 static void (*timer_interrupt_isr)(void) = NULL;
 
+/**
+ * The handler invoked when the SysTick timer goes off.
+ *
+ * Since we would like to customize the interrupt handler invoked, and also
+ * disable the SysTick mechanism as needed, this handler is effectively a wrapper for invoking
+ * #timer_interrupt_isr. 
+ * 
+ * If the latter is set to `NULL`, nothing happens; the timer interrupt is effectively disabled.
+ */
 void SysTick_Handler(void) {
 	if (timer_interrupt_isr != NULL) {
 		timer_interrupt_isr();
--- code/src/timer.h
+++ code/src/timer.h
@@ -1,4 +1,15 @@
 #pragma once
-
+/**
+ * Enables a timer interrupt which triggers \p timer_freq times a second,
+ * and invokes \p timer_callback when it is triggered.
+ *
+ * Internally, the SysTick mechanism is used.
+ *
+ * @param timer_callback The interrupt handler invoked when the timer goes off.
+ * @param timer_freq The frequency with which the timer goes off.
+ */
 void timer_enable(void (*timer_callback)(void), float timer_freq);
+
+/** Disables any currently registered timer interrupts enabled with timer_enable()
+ */
 void timer_disable(void);
