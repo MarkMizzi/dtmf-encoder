fe647609900da9896f3a7a8db6f499aca08faba1
gapa0002 <gabrielapap@hotmail.co.uk>
2023-03-28 09:42:59 +0000

    Adding LCD code

--- code/src/main.c
+++ code/src/main.c
@@ -1,9 +1,12 @@
 #include "dac_interrupt.h"
 #include "dtmf_symbols.h"
 #include "queue.h"
+#include "lcd.h"
 
 #include <dac.h>
 #include <system_LPC407x_8x_177x_8x.h>
+
+char char_array[16] = {'1', '2', '3', 'A', '4', '5', '6', 'B', '7', '8', '9', 'C', '*', '0', '#', 'D'};
 
 void start_or_enqueue(int symbol);
 
@@ -11,12 +14,16 @@ void start_or_enqueue(int symbol) {
 	if (!dac_interrupt_enable(COL(symbol), ROW(symbol))) {
 		enqueue(symbol);
 	}
+	lcd_put_char(char_array[symbol]);
 }
 
 int main(void) {
 	volatile int i;
 	dac_init();
+	lcd_init();
+	lcd_clear();
 	__enable_irq();
+	
 	start_or_enqueue(SYMBOL_0);
 	for (i = 0; i < 1000000; i++);
 	start_or_enqueue(SYMBOL_8);
