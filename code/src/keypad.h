557141a811d8c082776f4836d70605a78c8e9dd7
dfil0003 <dakifile03@gmail.com>
2023-04-20 14:49:28 +0000

    trying new pins for keypad

--- code/src/keypad.h
+++ code/src/keypad.h
@@ -6,17 +6,17 @@
 
 //defining macros for the pins connected to the columns
 //32 - 29 columns
-#define P_COL_0 P5_2
-#define P_COL_1	P5_3
-#define P_COL_2 P1_2
-#define P_COL_3 P1_3
+#define P_COL_0 P1_3
+#define P_COL_1	P1_5
+#define P_COL_2 P1_6
+#define P_COL_3 P1_7
 
 //defining macros for the pins connected to the rows
 //28 - 25 rows
-#define P_ROW_0 P1_5
-#define P_ROW_1 P1_6
-#define P_ROW_2 P1_7
-#define P_ROW_3 P1_11
+#define P_ROW_0 P0_21
+#define P_ROW_1 P0_0
+#define P_ROW_2 P0_1
+#define P_ROW_3 P0_9
 
 #define ROWS 4
 #define COLS 4
