d3835c4527206510216a99a1baab33297b1bb5ad
mmiz0052 <mizzimark2001@gmail.com>
2023-05-09 16:51:36 +0000

    Added WFI instruction in main loop to send microcontroller to deep sleep while inactive.

--- code/src/main.c
+++ code/src/main.c
@@ -6,33 +6,9 @@
 #include <platform.h>
 #include <gpio.h>
 
-void keypad_callback(int sources) {
-	uint32_t pin_mask;
-	
-	gpio_set(P_DBG_ISR, 1);
-	
-	pin_mask = 
-		(1 << GET_PIN_INDEX(P_ROW_0)) |
-		(1 << GET_PIN_INDEX(P_ROW_1)) |
-		(1 << GET_PIN_INDEX(P_ROW_2)) |
-		(1 << GET_PIN_INDEX(P_ROW_3));
-
-	if (!(sources & pin_mask)) {
-		return;
-	}
-	
-	lcd_put_char('A');
-	gpio_set(P_DBG_ISR, 1);
-}
-	
-
 int main(void) {
 	
-	LPC_SC->PCONP &= ~(1 << 12); // Clear bit 12 to disable ADC
-	
 	keypad_set_read_callback(tone_play_or_enqueue);
-	
-	volatile int i = 0;
 	
 	lcd_init();
 	lcd_clear();
@@ -43,6 +19,6 @@ int main(void) {
 	keypad_init();
 	
 	while (1) {
-		gpio_toggle(P_DBG_MAIN);
+		__asm("WFI");
 	}
 }
