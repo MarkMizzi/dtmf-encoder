7739d897500304e61c3f58a2d9b842fc7e58844b
mmiz0052 <mizzimark2001@gmail.com>
2023-05-16 06:06:19 +0000

    Update Doxygen documentation
    Add documentation for main.c

--- code/src/main.c
+++ code/src/main.c
@@ -7,17 +7,37 @@
 #include <lpc_eeprom.h>
 #include <platform.h>
 
+/** \brief Global variable containing system settings.
+ */
 Settings current;
 
+/** \brief Index of power bit for Timer 1 peripheral in `LPC_SC->PCONP`.
+ */
 #define PCTIM1 2
+/** \brief Index of power bit for UART 0 peripheral in `LPC_SC->PCONP`.
+ */
 #define PCUART0 3
+/** \brief Index of power bit for UART 1 peripheral in `LPC_SC->PCONP`.
+ */
 #define PCUART1 4
+/** \brief Index of power bit for I2C controller 0 peripheral in `LPC_SC->PCONP`.
+ */
 #define PCI2C0 7
+/** \brief Index of power bit for I2C controller 1 peripheral in `LPC_SC->PCONP`.
+ */
 #define PCI2C1 19
+/** \brief Index of power bit for I2C controller 2 peripheral in `LPC_SC->PCONP`.
+ */
 #define PCI2C2 26
+/** \brief Index of power bit for RTC timer peripheral in `LPC_SC->PCONP`.
+ */
 #define PCRTC 9
-#define PCGPIO 15
 
+/** \brief Powers down unneeded peropherals.
+ *
+ * This is called at system start-up in order to maximize power efficiency.
+ * Only peripherals which are initially on are considered.
+ */
 void power_down_peripherals()
 {
 	LPC_SC->PCONP &= ~(1 << PCTIM1);
